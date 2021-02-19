bfabe62c38a5933184d44f9216765df70b24bd23
Hanna <hanna-swida@hotmail.com>
2021-02-19 16:34:34 +0100

    the other version from my laptop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from loader import Loader
-from model import Model
+from Loader import Loader
+from Model import Model
 import torch
 import torch.optim as optim
 import constants as consts
@@ -7,17 +7,17 @@ import constants as consts
 def main():
     loader = Loader("training-data/voxceleb")
     model = Model(len(loader.speakers))
-    #optimizer = optim.RMSprop(model.parameters(), lr=consts.learning_rate, alpha=consts.alpha, eps=1e-07)
+    optimizer = optim.RMSprop(model.parameters(), lr=consts.learning_rate, alpha=consts.alpha, eps=1e-07)
 
     model.train()
 
     for epoch in range(100):
         batch, speakers = loader.next()
-        #optimizer.zero_grad()
+        optimizer.zero_grad()
         loss = model(torch.tensor(batch), torch.tensor(speakers, dtype=torch.long))
         print(loss)
         loss.backward()
-        #optimizer.step()
+        optimizer.step()
 
 if __name__ == "__main__":
     main()
