26d8f89a079f360c019aa1942bac7200ab2203bd
Hanna Swida <hanna-swida@hotmail.com>
2022-09-06 21:50:16 +0200

    d-vectors for cpu WIP

--- d-vectors-vox.py
+++ d-vectors-vox.py
@@ -2,9 +2,9 @@ import torch
 from model import Model
 from vox_celeb_loader import VoxLoaderDvector
 
-path = "C:/Users/hanna/POLIBUDA/MAGISTERKA/Magisterka_python/checkpoints_voxceleb/checkpoint_e0.pth.tar"
+path = "C:/Users/hanna/POLIBUDA/MAGISTERKA/Magisterka_python/checkpoints_voxceleb/checkpoint_test176.pth.tar"
 with open("voxdebug.txt", "a") as o:
-    voices_loader = VoxLoaderDvector('training-data/voxceleb')
+    voices_loader = VoxLoaderDvector('training-data/test')
     o.write("voices loader: \n ")
     o.write(str(len(voices_loader)))
     speaker_utts_arr = []
@@ -14,7 +14,7 @@ with open("voxdebug.txt", "a") as o:
         name_arr.append(speaker_name)
     x = torch.stack(speaker_utts_arr)
     cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-    checkpoint = torch.load(path)
+    checkpoint = torch.load(path,map_location=torch.device('cpu'))
 
     model = Model(len(voices_loader.speakers))
     model.to("cpu")
@@ -40,6 +40,5 @@ with open("voxdebug.txt", "a") as o:
     plt.title('Heatmap of S', fontsize=20)  # title with fontsize 20
 
     plt.ylabel('Speakers', fontsize=15)  # y-axis label with fontsize 15
-    print(len(name_arr))
-    ax = sns.heatmap(S, yticklabels=name_arr, linewidth=0.4,cmap="Blues")
+    ax = sns.heatmap(S, yticklabels=name_arr, linewidth=0.4,cmap="tab20_r")
     plt.show()
