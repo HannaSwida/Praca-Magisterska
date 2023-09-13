1a355a2938dbe24bbe473f2fd1956ded7989b9a5
Hanna Swida <hanna-swida@hotmail.com>
2023-09-13 10:28:50 +0200

    add bigger hidden layer

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -12,8 +12,9 @@ from model import Encoder
 import os
 
 
+
 class CustomMLP(nn.Module):
-    def __init__(self, input_dim, output_dim, hidden_dim=256):
+    def __init__(self, input_dim, output_dim, hidden_dim):
         super(CustomMLP, self).__init__()
         self.hidden_layer = nn.Linear(input_dim, hidden_dim)
         self.relu = nn.ReLU()
@@ -24,9 +25,8 @@ class CustomMLP(nn.Module):
         x = self.output_layer(x)
         return x
 
-
 def load_encoder(encoder_path):
-    checkpoint = torch.load(encoder_path, map_location=torch.device('cpu'))
+    checkpoint = torch.load(encoder_path)
     encoder = Encoder()
     encoder.load_state_dict(checkpoint)
     return encoder
@@ -34,7 +34,7 @@ def load_encoder(encoder_path):
 
 def extract_d_vectors(encoder, data_loader):
     d_vectors = []
-    labels = []
+    labels = []  # List to store the labels
     with torch.no_grad():
         for batch in data_loader:
             inputs, labels_batch = batch
@@ -47,51 +47,65 @@ def extract_d_vectors(encoder, data_loader):
 
 
 def main(argv):
-    n_batches = 3000
+    n_batches = 10000
     batch_size = 128
     n_workers = 1
-    n_epochs = 1000
+    # n_epochs = 200
+    n_epochs = 100
     lr = 0.001
     learning_rate_decay = 0.95
-    patience = 10000
+    patience = 100
     max_grad_norm = 1.0
-    print("setting end")
+    print("n_batches: ", n_batches, "batch_size: ", batch_size, "n ep: ", n_epochs, "patience: ", patience)
 
     input_dirs_file = '../../source_vox1dir.txt'
+    print(input_dirs_file)
     tr_data = VCset(input_dirs_file, n_batches=n_batches, chunk_size=0.4)
-    print("tr data init end")
     tr_loader = DataLoader(tr_data, batch_size=batch_size, pin_memory=True, num_workers=n_workers)
-    print("tr loader init end")
-    encoder_path = "../checkpoints/MiModel/04s/latest/vox1_1000B_1500/small/encoder_108.pth"
+    encoder_path = "../checkpoints/MiModel/04s/vox1_1000B_1500/small/encoder_141.pth"
 
     encoder = load_encoder(encoder_path)
     d_vectors, labels = extract_d_vectors(encoder, tr_loader)
 
     label_encoder = LabelEncoder()
     y_encoded = label_encoder.fit_transform(labels)
-    print("y_encoded end")
 
     X_train, X_test, y_train_encoded, y_test_encoded = train_test_split(d_vectors, y_encoded, test_size=0.2,
                                                                         random_state=42)
 
     input_dim = d_vectors.shape[1]
     output_dim = len(np.unique(labels))
+    hidden_dim = 8192
+    print("Hidden dimensions size: ", hidden_dim)
 
-    model = CustomMLP(input_dim, output_dim)
+    model = CustomMLP(input_dim, output_dim, hidden_dim)
+
+    """
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.5, nesterov=True)
-    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=learning_rate_decay)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.5, nesterov=True) 
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=learning_rate_decay) 
+    print("optimizer: SGD") 
+    """
 
-    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
-    y_train_tensor = torch.tensor(y_train_encoded, dtype=torch.long)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=learning_rate_decay)
+    print("optimizer: Adam")
+
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    y_test = torch.tensor(y_test_encoded, dtype=torch.long)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_train = torch.tensor(y_train_encoded, dtype=torch.long)
+    print("TESTED WITH test set")
 
     best_loss = float('inf')
     patience_counter = 0
+    print("Training epochs start...")
     for epoch in range(n_epochs):
         model.train()
         optimizer.zero_grad()
-        outputs = model(X_train_tensor)
-        loss = criterion(outputs, y_train_tensor)
+        outputs = model(X_train)
+        loss = criterion(outputs, y_train)
         loss.backward()
 
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
@@ -99,7 +113,7 @@ def main(argv):
         optimizer.step()
         scheduler.step()
 
-        if epoch % 5 == 0:
+        if epoch % 2 == 0:
             print(f"Epoch: {epoch}/{n_epochs}, Loss: {loss.item()}")
 
         if loss.item() < best_loss:
@@ -109,15 +123,57 @@ def main(argv):
             patience_counter += 1
 
         if patience_counter >= patience:
-            print("Stop, żadnej poprawy od", patience, "epok")
+            print("Early stopping! No improvement in the last", patience, "epochs.")
             break
 
-    outputdir = '../checkpoints/MiClassifier/'
+        if (epoch + 1) % 5 == 0:
+            model.eval()
+            with torch.no_grad():
+                outputs = model(X_test)
+                _, predicted_labels = torch.max(outputs, 1)
+                accuracy = torch.sum(predicted_labels == torch.tensor(y_test, dtype=torch.long)).item() / len(y_test)
+                good = 0
+                label_mapping = {}
+                with torch.no_grad():
+                    outputs = model(X_test)
+                    _, predicted_labels = torch.max(outputs, 1)
+
+                    for i in range(len(y_test)):
+                        true_label = y_test[i].item()
+                        predicted_label = predicted_labels[i].item()
+                        label_mapping[predicted_label] = true_label
+                        if predicted_label == true_label:
+                            good += 1
+
+                print(f"Epoch {epoch + 1}:")
+                print("Accuracy:", accuracy)
+                print("Predicted Label to True Label Mapping:")
+                print(label_mapping)
+                print("Correct pairs: ", good, "/", len(label_mapping))
+
+                outputdir = '../checkpoints/MiClassifier/vox1res'
+                if outputdir != "":
+                    if not os.path.exists(outputdir):
+                        os.makedirs(outputdir)
+
+                    path = os.path.join(outputdir, "classifier_10000b_vox1" + str(epoch) + ".tar")
+
+                    model_state_dict = model.state_dict()
+
+                    torch.save({
+                        'epoch': epoch,
+                        'model_state_dict': model_state_dict,
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        # 'loss': loss,
+                        'nlabels': tr_data.get_nlabels()
+                    }, path)
+
+    outputdir = '../checkpoints/MiClassifier/vox1res'
     if outputdir != "":
         if not os.path.exists(outputdir):
             os.makedirs(outputdir)
 
-        path = os.path.join(outputdir, "sss" + str(epoch) + ".tar")
+        path = os.path.join(outputdir, "vox1Classifier" + str(epoch) + ".tar")
 
         model_state_dict = model.state_dict()
 
@@ -130,17 +186,37 @@ def main(argv):
         }, path)
 
     model.eval()
-    X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
     with torch.no_grad():
-        outputs = model(X_test_tensor)
+        outputs = model(X_test)
+        print("OUTPUTS", outputs)
         _, predicted_labels = torch.max(outputs, 1)
+        print("predicted labels: ", predicted_labels)
+        print(
+            "*****************************************************0.4s**************************************************************")
         print(
             "*******************************************************************************************************************")
         print(
             "*******************************************************************************************************************")
-        print("suma dobrze przewidzianych: ",
-              torch.sum(predicted_labels == torch.tensor(y_test_encoded, dtype=torch.long)).item())
-        print("Len: ", len(y_test_encoded))
-        accuracy = torch.sum(predicted_labels == torch.tensor(y_test_encoded, dtype=torch.long)).item() / len(
-            y_test_encoded)
-        print("Dokładność:", accuracy)
+        print("predicted labels sum: ", torch.sum(predicted_labels == torch.tensor(y_test, dtype=torch.long)).item())
+        print("Len: ", len(y_test))
+        accuracy = torch.sum(predicted_labels == torch.tensor(y_test, dtype=torch.long)).item() / len(y_test)
+        print("Accuracy:", accuracy)
+        label_mapping = {}
+
+        with torch.no_grad():
+            outputs = model(X_test)
+            print("OUTPUTS", outputs)
+            _, predicted_labels = torch.max(outputs, 1)
+
+            for i in range(len(y_test)):
+                true_label = y_test[i].item()
+                predicted_label = predicted_labels[i].item()
+
+                label_mapping[predicted_label] = true_label
+
+            print("Predicted Label to True Label Mapping:")
+            print(label_mapping)
+
+
+if __name__ == "__main__":
+    main(None)
